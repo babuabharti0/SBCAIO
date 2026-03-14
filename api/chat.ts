8a8aec161b428262acb319b6e02671908825868b
babuabharti0 <babuabharti0@gmail.com>
2026-03-14 10:35:54 +0530

    Update chat.ts

--- api/chat.ts
+++ api/chat.ts
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import { companyKnowledge } from "../src/data/companyKnowledge.js";
+import { companyKnowledge } from "../src/data/companyKnowledge";
 
 export default async function handler(req, res) {
 
